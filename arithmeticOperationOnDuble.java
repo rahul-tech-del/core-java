eb88497def38f619a9a0b71a5596cc2cea1b70bc
Rahul Patel <patelr2294@gmail.com>
2026-06-10 03:00:02 +0530

    Create arithmeticOperationOnDuble.java

---
+++ arithmeticOperationOnDuble.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class arithmeticOperationOnDuble {
+    public static void main(String[] args) {
+        double x = 5, y = 2;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+    }
+    
+}
