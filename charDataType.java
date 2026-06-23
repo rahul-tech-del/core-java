e611cdc280eb2d204c8e2df736453732a134509f
Rahul Patel <patelr2294@gmail.com>
2026-06-23 08:09:55 +0530

    Create charDataType.java

---
+++ charDataType.java
@@ -0,0 +1,14 @@
+package basics;
+
+public class charDataType {
+    public static void main(String[] args) {
+        //character data type 
+        // a b c d ....z
+        //A B C D ....Z 
+        //0 1 2 3 ....9
+        //! @ # $ % ^ & * ....
+        char x = 'H';
+        System.out.println(x);
+    }
+    
+}
