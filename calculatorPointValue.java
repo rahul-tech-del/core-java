42aa4299fdaf4a25b12f093aed78aedfe277d124
Rahul Patel <patelr2294@gmail.com>
2026-06-30 19:32:05 +0530

    Create calculatorPointValue.java

---
+++ calculatorPointValue.java
@@ -0,0 +1,25 @@
+// Simple Java Calculator
+// Uses Scanner for input and switch statement for arithmetic operations
+
+
+package Conditionals;
+
+import java.util.Scanner;
+
+public class calculatorPointValue {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            float a = sc.nextInt();
+            char op = sc.next().charAt(0);
+            float b = sc.nextInt();
+            switch (op){
+                case '+' -> System.out.println(a+b);
+                case '-' -> System.out.println(a-b);
+                case '*' -> System.out.println(a*b);
+                case '/' -> System.out.println(a/b);
+            }
+        } 
+
+    }
+    
+}
