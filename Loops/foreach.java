40310bac75acbb91369e2230f23ff5bd753fe62c
rahul-tech-del <patelr22942gmail.com>
2026-07-24 01:03:54 +0530

    for each loop

---
+++ Loops/foreach.java
@@ -0,0 +1,10 @@
+package Loops;
+
+public class foreach {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50};
+        for(int ele : arr){
+            System.out.print(ele+" ");
+        }
+    }
+}
